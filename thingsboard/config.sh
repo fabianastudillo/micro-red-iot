0f7789518299a5c5f08404efba3ab2fe25dab54f
Fabian Astudillo <fabian.astudillo.salinas@gmail.com>
2024-01-25 12:23:26 -0500

    nuevos archivos: thingsboard/config.sh 	nuevos archivos: thingsboard/docker-compose.yml

---
+++ thingsboard/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p ~/.microgridtb-edge-data && sudo chown -R 799:799 ~/.microgridtb-edge-data
+mkdir -p ~/.microgridtb-edge-logs && sudo chown -R 799:799 ~/.microgridtb-edge-logs
+mkdir -p ~/.microgridtb-data && sudo chown -R 799:799 ~/.microgridtb-data
+mkdir -p ~/.microgridtb-logs && sudo chown -R 799:799 ~/.microgridtb-logs
+
